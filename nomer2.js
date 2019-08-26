fc48027813b64e295179d913a77f093c079da73c
Aditya Permana <54143766+aditperm@users.noreply.github.com>
2019-08-26 15:41:23 +0700

    Add files via upload

---
+++ nomer2.js
@@ -0,0 +1,26 @@
+//exercise2
+
+var name = 'arief'
+
+if (name=="arief"){
+  console.log("masuk group pertama")
+}
+else if (name=="rinnina"){
+  console.log("masuk group kedua")
+}
+else if (name=="joshua"){
+  console.log("masuk group terakkhir")
+}
+else if (name=="yusril"){
+  console.log("masuk group terakkhir")
+}
+else {
+  console.log("Nama tidak ada")
+}
+
+
+// Contoh:
+// - `name` = 'arief', output: 'Masuk group pertama'
+// - `name` = 'rinnina', output: 'Masuk group kedua'
+// - `name` = 'joshua', output: 'Masuk group terakhir'
+// - `name` = 'yusril', output: 'Masuk group terakhir'
---
+++ nomer3.js
@@ -0,0 +1,5 @@
+function addTitle (name1, name2, name3, name4) {
+  console.log('Mr.' + name1 + 'Mr.' + name2 + 'Mr.' + name3 + 'Mr.' + name4 );
+}
+
+addTitle('aa','ii','uu','ee','oo');
